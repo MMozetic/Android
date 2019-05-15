fb2fbada7b697dd07cca389c42ae93f1a6308215
student <student@domain.local>
2019-05-15 16:00:59 +0200

    idemoooo

--- app/src/main/java/pnrs/rtrk/myapplication/StatisticsActivity.java
+++ app/src/main/java/pnrs/rtrk/myapplication/StatisticsActivity.java
@@ -1,20 +1,28 @@
 package pnrs.rtrk.myapplication;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class StatisticsActivity extends AppCompatActivity {
 
     private String city, day, date;
-    private TextView cityView, imageEdit;
+    private TextView cityView, imageEdit, minDan, minTemp, maxDan, maxTemp;
 
     private ImageView snowImage, sunImage;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +46,30 @@ public class StatisticsActivity extends AppCompatActivity {
                 imageEdit.setText("Hladnooo");
             }
         });
-
         sunImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 imageEdit.setText("Toplooo");
             }
         });
-
         cityView.setText(city);
 
+        minDan = findViewById(R.id.minDan);
+        minTemp = findViewById(R.id.minTemp);
+        maxDan = findViewById(R.id.maxDan);
+        maxTemp = findViewById(R.id.maxTemp);
+
+        ContentResolver resolver = getContentResolver();
+        cursor = resolver.query(WeatherProvider.CONTENT_URI, null, "Name=?",new String[]{city},"Temperature ASC");
+        cursor.moveToFirst();
+
+        minDan.setText(cursor.getString(cursor.getColumnIndex("Date")));
+        minTemp.setText(Double.toString(cursor.getDouble(cursor.getColumnIndex("Temperature"))));
+
+        cursor.moveToLast();
+        maxDan.setText(cursor.getString(cursor.getColumnIndex("Date")));
+        maxTemp.setText(Double.toString(cursor.getDouble(cursor.getColumnIndex("Temperature"))));
+
+        cursor.close();
     }
 }
