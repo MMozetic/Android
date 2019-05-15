3417f2324d817195caa48e3a69043954e5a6220f
MMozetic <mozeticmilos97@gmail.com>
2019-05-15 12:17:35 +0200

    uradjen novi aktiviti skoro

--- app/src/main/java/pnrs/rtrk/myapplication/StatisticsActivity.java
+++ app/src/main/java/pnrs/rtrk/myapplication/StatisticsActivity.java
@@ -2,6 +2,8 @@ package pnrs.rtrk.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -10,7 +12,9 @@ import java.util.Calendar;
 public class StatisticsActivity extends AppCompatActivity {
 
     private String city, day, date;
-    private TextView t1,t2,t3;
+    private TextView cityView, imageEdit;
+
+    private ImageView snowImage, sunImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +26,27 @@ public class StatisticsActivity extends AppCompatActivity {
         day = bundle.get("day").toString();
         date =  bundle.get("date").toString();
 
-        t1 = findViewById(R.id.cityText);
-        t2 = findViewById(R.id.dayText);
-        t3 = findViewById(R.id.dateText);
+        cityView = findViewById(R.id.cityText);
 
-        t1.setText(city);
-        t2.setText(day);
-        t3.setText(date);
+        imageEdit = findViewById(R.id.imageEdit);
+        snowImage = findViewById(R.id.snowImage);
+        sunImage = findViewById(R.id.sunImage);
+
+        snowImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageEdit.setText("Hladnooo");
+            }
+        });
+
+        sunImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageEdit.setText("Toplooo");
+            }
+        });
+
+        cityView.setText(city);
+
     }
 }
